ba1f7e6a7f9685f0fb423b25e4409e93f74b140f
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-03-27 17:03:29 +0530

    UI work is done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import prism from "prismjs"; 
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(()=>{
+    prism.highlightAll();
+  })
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className='left'>
+        <div className='code'>
+          <pre>
+            <code className='language-javascript'>
+              {
+                `
+                function sum(){
+                return 1+1
+                }
+                `
+              }
+            </code>
+          </pre>
+
+        </div>
+        <div className='review'>
+          Review 
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='right'>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main>
     </>
   )
+}
+
+//dummy function
+function sum(){
+  return 1+1;
 }
 
 export default App
